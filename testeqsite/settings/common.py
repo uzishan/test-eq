7f0095392dc7a370ae4e788a9ec10ca791e9b853
Lucian Corduneanu <lucian.corduneanu@sensidev.com>
2016-02-19 18:26:23 +0200

    Adjust project root.

--- testeqsite/settings/common.py
+++ testeqsite/settings/common.py
@@ -106,7 +106,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
