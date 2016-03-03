935b63cb035ab16acb60fcd800eab096cc931cac
Uzishan(Marius S) <blood.angel66797@gmail.com>
2016-03-03 21:42:40 +0200

    2 small typos
    forgot : in 2 places

--- core/models.py
+++ core/models.py
@@ -22,7 +22,7 @@ class Room(models.Model):
     surface = models.CharField(max_length=30)
     floor = models.ForeignKey('Floor', on_delete=models.CASCADE)
 
-class Sensor(models.Model)
+class Sensor(models.Model):
     name = models.CharField(max_length=30)
     serial_no = models.IntegerField()
     type = models.CharField(max_length=30)
@@ -30,7 +30,7 @@ class Sensor(models.Model)
     unit_of_measurement = models.CharField(max_length=10)
     room = models.ForeignKey('Room', on_delete=models.CASCADE)
 
-class SensorData(models.Model)
+class SensorData(models.Model):
     sensor = models.ForeignKey('Sensor', on_delete=models.CASCADE)
     timestamp = models.DateTimeField('Read date', auto_now=True)
     value = models.IntegerField()
