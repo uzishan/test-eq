553a1475e9c0d1552c034cf8bc7b3bed6b4a24f1
Uzishan(Marius S) <blood.angel66797@gmail.com>
2016-03-03 21:39:32 +0200

    models
    Created models ready for adminapp

--- testeqsite/settings/common.py
+++ testeqsite/settings/common.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
