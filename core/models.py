44210213b0ad47e09fcf826b0eed79d9e6605304
Lucian Corduneanu <lucian.corduneanu@sensidev.com>
2016-03-09 23:06:48 +0200

    Adjust models PEP8 and default env var #2

--- core/models.py
+++ core/models.py
@@ -1,26 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Building(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=50)
-    image_url = models.ImageField()
+    image_url = models.CharField(max_length=255)
     location_address = models.CharField(max_length=100)
     latitude = models.IntegerField()
     longitude = models.IntegerField()
 
+
 class Floor(models.Model):
     number = models.IntegerField()
     title = models.CharField(max_length=30)
-    plans_image_url = models.ImageField()
-    building = models.ForeignKey('Building',on_delete=models.CASCADE)
+    plans_image_url = models.CharField(max_length=255)
+    building = models.ForeignKey('Building', on_delete=models.CASCADE)
+
 
 class Room(models.Model):
     title = models.CharField(max_length=30)
     type = models.CharField(max_length=30)
     surface = models.CharField(max_length=30)
     floor = models.ForeignKey('Floor', on_delete=models.CASCADE)
+
 
 class Sensor(models.Model):
     name = models.CharField(max_length=30)
@@ -30,9 +34,8 @@ class Sensor(models.Model):
     unit_of_measurement = models.CharField(max_length=10)
     room = models.ForeignKey('Room', on_delete=models.CASCADE)
 
+
 class SensorData(models.Model):
     sensor = models.ForeignKey('Sensor', on_delete=models.CASCADE)
     timestamp = models.DateTimeField('Read date', auto_now=True)
     value = models.IntegerField()
-    
-    
--- core/views.py
+++ core/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, User. You are at the Core index")
+    return HttpResponse("Hello, User. You are at the Core index!")
