0b5c04d2738328dce95fba307d7471c6507d27fd
Uzishan(Marius S) <blood.angel66797@gmail.com>
2016-04-11 15:38:49 +0300

    more data added
    more data trying to complete admin building.

--- core/admin.py
+++ core/admin.py
@@ -2,9 +2,23 @@ from django.contrib import admin
 
 from core.models import Building, Floor, Room, Sensor, SensorData
 
+class SensorDataInline(admin.TabularInline):
+    model = SensorData
 
+class SensorInline(admin.TabularInline):
+    model = Sensor
+    inlines = [SensorDataInline]
+
+class RoomInline(admin.TabularInline):
+    model = Room
+    inlines = [SensorInline]
+
+class FloorInline(admin.TabularInline):
+    model = Floor
+    inlines = [RoomInline]
 
 class BuildingAdmin(admin.ModelAdmin):
+    inlines = [FloorInline]
     pass
 
 bModels = [models.Building, models.Floor, models.Room, models.Sensor, models.SensorData]
