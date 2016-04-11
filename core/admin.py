2ae36c612019b1fe37fdf3074640d46e02489289
Uzishan(Marius S) <blood.angel66797@gmail.com>
2016-04-11 15:03:07 +0300

    admin for all models
    added admin for all models under isterable list bModels

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from core.models import Building
+from core.models import Building, Floor, Room, Sensor, SensorData
+
 
 
 class BuildingAdmin(admin.ModelAdmin):
     pass
 
-
-admin.site.register(Building, BuildingAdmin)
+bModels = [models.Building, models.Floor, models.Room, models.Sensor, models.SensorData]
+admin.site.register(bModels)
