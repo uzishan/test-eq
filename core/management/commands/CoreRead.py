f0bc1c9c6431901326fd91fdf272c9aa2488f3c8
Uzishan(Marius S) <blood.angel66797@gmail.com>
2016-04-14 21:33:03 +0300

    added basics for custom command
    added basics for custom command

---
+++ core/management/commands/CoreRead.py
@@ -0,0 +1,9 @@
+
+from django.core.management.base import BaseCommand, CommandError
+from core.models import SensorData
+
+class Command(BaseCommand)
+    help = 'Simulates data collection for Humidity and Temperature sensors'
+
+
+    # def handle(self.....) not sure how to write this.
