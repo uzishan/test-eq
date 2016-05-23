053888c9435a66d23edfc5a44d116d312eb35092
uzishan <blood.angel66797@gmail.com>
2016-05-23 18:47:20 +0300

    code added to CoreRead altough a bit far from being complete. few errors here and there but this is the basic code

--- core/management/commands/CoreRead.py
+++ core/management/commands/CoreRead.py
@@ -9,8 +9,11 @@ class Command(BaseCommand):
     help = 'Simulates data collection for Humidity and Temperature sensors'
 
     def handle(self):
+
             for Sensor.id in range(0, 1000):
+
                 if Sensor.type == "temperature":
+
                     return Sensor.SensorData.value(randint(20, 40))
                 else:
                     return Sensor.SensorData.value(randint(5, 10))
