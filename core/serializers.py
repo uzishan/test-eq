0fe52106d3d7d0f177ec3f1803fcb1d5f29655fd
Lucian Corduneanu <lucian.corduneanu@sensidev.com>
2016-05-26 01:07:29 +0300

    Add placeholder for avg temp

--- core/serializers.py
+++ core/serializers.py
@@ -3,13 +3,21 @@ from rest_framework import serializers
 
 
 class BuildingSerializer(serializers.HyperlinkedModelSerializer):
+    location = serializers.CharField(source='location_address')
+    avg_temperature = serializers.SerializerMethodField('_get_avg_temperature')
+
     class Meta:
         model = Building
         fields = ('pk',
                   'title',
                   'image_url',
                   'location',
-                  'campus_name')
+                  'campus_name',
+                  'avg_temperature')
+
+    def _get_avg_temperature(self, obj):
+        # magia filtrului per buildingu asta.
+        return "media temperaturii pe buildingu asta!"
 
 
 class FloorSerializer(serializers.HyperlinkedModelSerializer):
