0ddec0a8c09cfddad6dc64f1cb658e4796ca45c1
uzishan <blood.angel66797@gmail.com>
2016-05-26 01:39:58 +0300

    avg_humid,avg_temp,Occupancy(bugfixing)

--- core/serializers.py
+++ core/serializers.py
@@ -1,10 +1,14 @@
 from core.models import *
 from rest_framework import serializers
+from random import randint
+from django.db.models import Avg
 
 
 class BuildingSerializer(serializers.HyperlinkedModelSerializer):
     location = serializers.CharField(source='location_address')
     avg_temperature = serializers.SerializerMethodField('_get_avg_temperature')
+    avg_humidity = serializers.SerializerMethodField('_get_avg_humidity')
+    occupancy_level = serializers.SerializerMethodField('o_c')
 
     class Meta:
         model = Building
@@ -13,11 +17,23 @@ class BuildingSerializer(serializers.HyperlinkedModelSerializer):
                   'image_url',
                   'location',
                   'campus_name',
-                  'avg_temperature')
+                  'avg_temperature',
+                  'avg_humidity',
+                  'occupancy_level')
 
-    def _get_avg_temperature(self, obj):
+    def _get_avg_temperature(self):
         # magia filtrului per buildingu asta.
-        return "media temperaturii pe buildingu asta!"
+        t = SensorData.objects.filter(Building_pk__in=Building.pk).filter(Sensor_type="temperature").aggregate(Avg('value'))
+        return t
+
+    def _get_avg_humidity(self):
+        # magia filtrului per buildingu asta.
+        h = SensorData.objects.filter(Building_pk__in=Building.pk).filter(Sensor_type="humidity").aggregate(Avg('value'))
+        return h
+
+    def o_c(self):
+        ocup = randint(45, 65)
+        return ocup
 
 
 class FloorSerializer(serializers.HyperlinkedModelSerializer):
