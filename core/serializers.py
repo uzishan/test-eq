efef5298e61c3f0902291e93529826600a5b28f3
uzishan <blood.angel66797@gmail.com>
2016-05-26 01:59:13 +0300

    more experimenting

--- core/serializers.py
+++ core/serializers.py
@@ -23,12 +23,12 @@ class BuildingSerializer(serializers.HyperlinkedModelSerializer):
 
     def _get_avg_temperature(self, obj):
         # magia filtrului per buildingu asta.
-        t = SensorData.objects.filter(Building_id__in=Building.pk).filter(Sensor_type="temperature").aggregate(Avg('value'))
+        t = SensorData.objects.filter(Sensor_type="temperature").aggregate(Avg('value'))
         return t
 
     def _get_avg_humidity(self, obj):
         # magia filtrului per buildingu asta.
-        h = obj.SensorData.objects.filter(Building_id__in=Building.pk).filter(Sensor_type="humidity").aggregate(Avg('value'))
+        h = obj.SensorData.objects.filter(Sensor_type="humidity").aggregate(Avg('value'))
         return h
 
     def o_c(self, obj):
