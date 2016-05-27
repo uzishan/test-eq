24aeea83a2bede9a156ae53a29de95c18f6fa17b
uzishan <blood.angel66797@gmail.com>
2016-05-27 19:51:35 +0300

    updated serializers
    updated avg_humidity
    occupancy level brought up to standard
    and sensor__type filter fixed.
    had to do this here as pycharm encountered 503 github unicorns!

--- core/serializers.py
+++ core/serializers.py
@@ -7,8 +7,8 @@ from django.db.models import Avg
 class BuildingSerializer(serializers.HyperlinkedModelSerializer):
     location = serializers.CharField(source='location_address')
     avg_temperature = serializers.SerializerMethodField('_get_avg_temperature')
-    avg_humidity = serializers.SerializerMethodField('_generate_occupancy_level')
-    occupancy_level = serializers.SerializerMethodField('_generate_occupancy_level')
+    avg_humidity = serializers.SerializerMethodField('_get_avg_humidity')
+    occupancy_level = serializers.SerializerMethodField('_get_occupancy_level')
 
     class Meta:
         model = Building
@@ -23,15 +23,15 @@ class BuildingSerializer(serializers.HyperlinkedModelSerializer):
 
     def _get_avg_temperature(self, obj):
         # magia filtrului per buildingu asta.
-        temp = SensorData.objects.filter(sensor__room__floor__building__pk=obj.pk).filter(sensor__type="temperature").aggregate(Avg('value'))
+        temp = SensorData.objects.filter(sensor__room__floor__building__pk=obj.pk).filter(sensor__type='TP').aggregate(Avg('value'))
         return temp
 
     def _get_avg_humidity(self, obj):
         # magia filtrului per buildingu asta.
-        hum = SensorData.objects.filter(sensor__room__floor__building__pk=obj.pk).filter(sensor__type="humidity").aggregate(Avg('value'))
+        hum = SensorData.objects.filter(sensor__room__floor__building__pk=obj.pk).filter(sensor__type='HU').aggregate(Avg('value'))
         return hum
 
-    def _generate_occupancy_level(self, obj):
+    def _get_occupancy_level(self, obj):
         ocup = randint(45, 65)
         return ocup
 
