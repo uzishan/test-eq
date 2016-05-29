579293d58cd0ac6d2965cae165e7191dcca50579
uzishan <blood.angel66797@gmail.com>
2016-05-29 16:51:58 +0300

    adding buildings/1/floors(experimental)

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
-from core import serializers
-
+from core import views
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^buildings/(?P<pk>[0-9]+)/$', serializers.BuildingSerializer.building_details_floor),
+    url(r'^buildings/(?P<pk>[0-9]+)/$', views.FloorViewSet),
 ]
