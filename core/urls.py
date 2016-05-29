59c09c03643ca9a1267343eb0dd39fa5d0b36354
uzishan <blood.angel66797@gmail.com>
2016-05-29 16:56:26 +0300

    adding buildings/1/floors(experimental)

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-from core import views
+
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^buildings/(?P<pk>[0-9]+)/$', views.FloorViewSet),
 ]
--- testeqsite/urls.py
+++ testeqsite/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include(router.urls))
+    url(r'^buildings/(?P<pk>[0-9]+)/$', views.BuildingViewSet, views.FloorViewSet)
 ]
