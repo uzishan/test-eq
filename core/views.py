0d2839d21355da51b0ab66881ebb07ed20e3a70b
uzishan <blood.angel66797@gmail.com>
2016-06-08 08:33:10 +0300

    fixing the floors showing on main page as well((experimenting/fixing)

--- core/views.py
+++ core/views.py
@@ -1,20 +1,25 @@
 
 from rest_framework import viewsets
-from core import serializers
+from core.serializers import *
 from core.models import *
 from django.http import HttpResponse
-from django.shortcuts import render
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
     return HttpResponse("Hello, User. You are at the Core index!")
 
 
-class BuildingViewSet(viewsets.ModelViewSet):
-    queryset = Building.objects.all()
+class BuildingViewSet(viewsets.ViewSet):
 
-    def get_serializer_class(self):
-        if self.action == 'list':
-            return serializers.BuildingSerializer
-        if self.action == 'retrieve':
-            return serializers.BuildingSubSerializer
+    def list(self, request):
+        queryset = Building.objects.all()
+        serializer = BuildingSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Building.objects.all()
+        building = get_object_or_404(queryset, pk=pk)
+        serializer = BuildingSubSerializer(building)
+        return Response(serializer.data)
