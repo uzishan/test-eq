655aecd79fc0b342b97cb307aedf98945db8337a
uzishan <blood.angel66797@gmail.com>
2016-06-08 08:52:29 +0300

    fixing the floors showing on main page as well((experimenting/fixing-dumb move)

--- testeqsite/urls.py
+++ testeqsite/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include(router.urls)),
-    url(r'^', include('buildings.urls')),
 ]
