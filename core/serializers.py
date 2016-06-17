fc62c26c6c3cb608fbe232e1dca2a2fe2fb9fbf5
uzishan <blood.angel66797@gmail.com>
2016-06-17 20:03:41 +0300

    this is the actual code(after a lot of useless experimenting)

--- core/serializers.py
+++ core/serializers.py
@@ -16,6 +16,7 @@ class BuildingSerializer(serializers.HyperlinkedModelSerializer):
     avg_temperature = serializers.SerializerMethodField('_get_avg_temperature')
     avg_humidity = serializers.SerializerMethodField('_get_avg_humidity')
     occupancy_level = serializers.SerializerMethodField('_get_occupancy_level')
+    floors = FloorSerializer(many=True)
 
     class Meta:
         model = Building
